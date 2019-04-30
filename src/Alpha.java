e3f3a721b864434dc33c6e4f5950505bb3cdf43e
mattzech <48557999+mattzech@users.noreply.github.com>
2019-04-30 11:36:09 +0200

    Update Alpha.java

--- src/Alpha.java
+++ src/Alpha.java
@@ -4,11 +4,24 @@ public class Alpha extends Theta{
     Alpha(){
         alpha = 0;
     }
+    /**
+     * Makes the variable "alpha" to be used in the formula
+     * @param multiple
+     * @param lambda
+     * @param width
+     * @param slitDistance
+     */
     Alpha(double multiple, double lambda, double width, double slitDistance){
         super(multiple, lambda, width);
         setAlpha(lambda, width, slitDistance);
     }
 
+    /**
+     * Calculates the variable
+     * @param lambda
+     * @param width
+     * @param slitDistance
+     */
     public void setAlpha(double lambda, double width, double slitDistance) {
         alpha = Math.PI * slitDistance * Math.sin(theta) / lambda;
     }
