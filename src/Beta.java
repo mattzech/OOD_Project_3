5187a3889b9d2d219f9e4c3ae9fe9052b6221933
mattzech <48557999+mattzech@users.noreply.github.com>
2019-04-30 11:37:50 +0200

    Update Beta.java

--- src/Beta.java
+++ src/Beta.java
@@ -5,11 +5,23 @@ public class Beta extends Theta {
         super();
         beta = 0;
     }
+    /**
+     * Sets up the variable beta to be used in the formula
+     * @param multiple
+     * @param lambda
+     * @param width
+     */
     Beta(double multiple, double lambda, double width){
         super(multiple, lambda, width);
         setBeta(multiple, lambda, width);
     }
 
+    /**
+     * Calculates the value using the formula
+     * @param multiple
+     * @param lambda
+     * @param width
+     */
     public void setBeta(double multiple, double lambda, double width) {
         setTheta(multiple, lambda, width);
         beta = Math.PI * width * Math.sin(theta) / lambda;
