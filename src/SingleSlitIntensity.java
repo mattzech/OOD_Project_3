b6bf4865306fcc54c01e2166b6ef2096720af0bb
mattzech <48557999+mattzech@users.noreply.github.com>
2019-04-30 11:46:17 +0200

    Update SingleSlitIntensity.java

--- src/SingleSlitIntensity.java
+++ src/SingleSlitIntensity.java
@@ -5,16 +5,32 @@ public class SingleSlitIntensity extends Beta {
         super();
         singleSlitIntensity = 0;
     }
+    /**
+     * Uses inheritance to create the single slit formula
+     * @param multiple
+     * @param lambda
+     * @param width
+     */
     SingleSlitIntensity(double multiple, double lambda, double width) {
         super(multiple, lambda, width);
         setSingleSlitIntensity(multiple, lambda, width);
     }
 
+    /**
+     * Calculates the value for single slit live so the slider can be adjusted
+     * @param multiple
+     * @param lambda
+     * @param width
+     */
     public void setSingleSlitIntensity(double multiple, double lambda, double width) {
         setBeta(multiple, lambda, width);
         singleSlitIntensity = (Math.sin(beta) / beta) * (Math.sin(beta) / beta);
     }
 
+    /**
+     * Returns the value
+     * @return
+     */
     public double getSingleSlitIntensity() {
         return singleSlitIntensity;
     }
